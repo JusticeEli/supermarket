d278088e54e5a82e8d74e810b50e3cada2902004
JusticeEli <justelonelin@gmail.com>
2020-06-08 17:10:18 +0300

    Initial commit

---
+++ app/src/main/java/com/justice/supermarket/SavedAddressActivity.java
@@ -0,0 +1,14 @@
+package com.justice.supermarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_address);
+    }
+}
